e11d5f46138dfc5ed045a29aec18407a70b5e367
Th3Mayar <josehenriquez.02.26@gmail.com>
2023-12-04 18:47:11 -0400

    Commit for checkout master and dowload changes

--- src/components/InputSearch/modal.tsx
+++ src/components/InputSearch/modal.tsx
@@ -1,41 +1,42 @@
-import { useState, ChangeEvent } from "react";
+import { SetStateAction, Dispatch, FormEventHandler } from "react";
+import "./modal.css";
+import { imagenes } from "../../../config/defaultConfig";
 
-interface ModalProps {
-  isOpen: boolean;
+type ModalProps = {
+  title: string;
   onClose: () => void;
-  extraProp: boolean; // Define el nombre y tipo de la propiedad adicional
-}
+  setState: Dispatch<SetStateAction<null | number | string>>;
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Modal = ({ isOpen, onClose, extraProp }: ModalProps) => {
-  const [stopPrice, setStopPrice] = useState<string>("");
-
-  const handleAccept = () => {
-    console.log("Stop Price:", stopPrice);
+const Modal: React.FC<ModalProps> = ({ title, onClose, setState }) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(form);
+    const priceStop = data["priceStop"] as string;
+    console.log(priceStop);
+    setState(priceStop);
     onClose();
-  };
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setStopPrice(e.target.value);
   };
 
   return (
     <>
-      {isOpen && (
-        <div className="overlay">
-          <div className="modal">
+      <div className="overlay">
+        <form className="modal relative" onSubmit={handleSubmit}>
+          <div className="w-[100%] flex flex-col items-center content-center gap-5">
             <span className="close-button" onClick={onClose}>
-              &times;
+              <i className="fas fa-times"></i>
             </span>
-            <h2>What will be your stop price?</h2>
+            <h2 className="titleModal">What will be your stop price?</h2>
             <input
+              name="priceStop"
               type="text"
               placeholder="US$0.00"
-              value={stopPrice}
-              onChange={handleInputChange}
+              className="inputPrice w-[100%]"
             />
             <div className="buttons">
-              <button className="button accept" onClick={handleAccept}>
+              <button type="submit" id="accept" className="button">
                 Accept
               </button>
               <button className="button cancel" onClick={onClose}>
@@ -43,10 +44,13 @@ const Modal = ({ isOpen, onClose, extraProp }: ModalProps) => {
               </button>
             </div>
           </div>
-        </div>
-      )}
+          <div className="flex w-[100%]">
+            <img src={imagenes.logo}/>
+          </div>
+        </form>
+      </div>
     </>
   );
-}
+};
 
 export default Modal;

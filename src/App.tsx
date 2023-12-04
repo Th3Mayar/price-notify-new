c2577b0096ede2a09bb3dff88799e63400411967
Th3Mayar <josehenriquez.02.26@gmail.com>
2023-12-03 21:12:52 -0400

    Agregando rutas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Dashboard from "./page/dashboard";
 import NavBarOut from "./components/Navbar/navbarOut";
 import Login from "./components/Login/SignIn/Login";
 import Register from "./components/Login/SignUp/Register";
+import Products from "./page/products";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/products" element={<Products />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/page/products.tsx
@@ -0,0 +1,11 @@
+import Products from "../components/Products/products";
+
+const ProductsPage = () => {
+  return (
+    <>
+      <Products />
+    </>
+  );
+};
+
+export default ProductsPage;

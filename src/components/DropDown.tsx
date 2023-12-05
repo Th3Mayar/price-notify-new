e19a7a3074d6d630c87e60640d2ede71d81f5fc4
WanderJ08 <wanderroque01@gmail.com>
2023-12-05 13:23:52 -0400

    cambios en el dropdown

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/userContext";
 import { useContext } from "react";
-
+import React from "react";
 const DropDown = () => {
   const navigation = [
     { label: "Edit profile", href: "/configProfile", icon: "fas fa-user", onClick: () => {}},
@@ -15,27 +15,30 @@ const DropDown = () => {
   const { deleteToken } = useContext(UserContext);
 
   return (
-    <section className=" absolute top-[100%] translate-y-[10px] z-[999] flex left-[-10px]">
+    <section className="absolute top-[100%] translate-y-[10px] z-[999] flex left-[-10px]">
       <nav className='contains'>
         <div className='iconbar'>
           <ul className='items'>
             {navigation.map((item, index) => (
-              <li
-                key={index}
-                className="rounded-full text-white flex items-center text-center space-x-2 py-1 px-4 hover:rounded-full hover:bg-white hover:bg-opacity-20"
-                onClick={item.onClick}
-              >
-                <i className={`w-5 ${item.icon}`}/>{" "}
-                <Link to={item.href}>
-                  <a className="text-white hover:text-indigo-f">{item.label}</a>
-                </Link>
-              </li>
+              <React.Fragment key={index}>
+                <li
+                  className="rounded-full text-white flex items-center text-center space-x-2 py-1 px-4 hover:rounded-full hover:bg-white hover:bg-opacity-20"
+                  onClick={item.onClick}
+                >
+                  <i className={`w-5 ${item.icon}`}/>{" "}
+                  <Link to={item.href}>
+                    <a className="text-white hover:text-indigo-f">{item.label}</a>
+                  </Link>
+                </li>
+                {index !== navigation.length - 1 && <hr />}
+              </React.Fragment>
             ))}
           </ul>
         </div>
       </nav>
     </section>
   );
+  
 };
 
 export default DropDown;

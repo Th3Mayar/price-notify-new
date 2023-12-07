2054955c5b3f82036098bc84ff375130cad6d1b4
Th3Mayar <josehenriquez.02.26@gmail.com>
2023-12-07 11:35:39 -0400

    Fix connection for compiled run

--- config/apollo.ts
+++ config/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-        uri: 'http://localhost:4000',
+        uri: 'https://api-price-notify.onrender.com',
         fetch
     })
 })
